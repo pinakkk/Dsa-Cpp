714dd81e94906dfdcd4537488fd658e3a39b9281
Pinak Kundu <pinakkundu1080@gmail.com>
2024-11-22 18:15:33 +0000

    restructured heapify algo

--- Heaps/Algos/heap.cpp
+++ Heaps/Algos/heap.cpp
@@ -1,4 +1,3 @@
-// You are using GCC
 #include <iostream> 
 using namespace std;
 
@@ -6,21 +5,24 @@ class heap{
     public:
     int arr[100];
     int size = 0;
-    
+
     heap(){
         arr[0] = -1;
-        size = 0;
+        size =0;
     }
-    
+
+    // Max Heap
     void insert(int val){
+        // 1. insert at last index 
+        // 2. compare with parent (index/2)
+
+        // increase the size to insert at last index
         size++;
-        
         int index = size;
         arr[index] = val;
-        
-        while(index>1){
+
+        while(index > 1){
             int parent = index/2;
-            
             if(arr[parent] < arr[index]){
                 swap(arr[parent], arr[index]);
                 index = parent;
@@ -30,121 +32,79 @@ class heap{
             }
         }
     }
-    
+
     void print(){
-        for(int i = 1; i<=size; i++){
+        for(int i=1; i<size; i++){
             cout << arr[i] << " ";
-        }
-        cout << endl;
+        } cout << endl;
     }
-    
-    void deletefromheap(){
-        if(size==0){
-            cout << "no delete" << endl;
+
+    void deleteFromHeap(){
+        // 1. swap last index with root 
+        // 2. remove last node
+        // 3. to correct position
+        if(size == 0){
+            cout << "nothing to delete";
             return;
         }
-        
+
+        // 1.
         arr[1] = arr[size];
+
+        // 2.
         size--;
-        
-        int i=1; 
-        
+
+        // to correct position
+        int i=1;
         while(i<size){
-            int left = 2 * i;
-            int right = (2*i) + 1;
-            
-            if(left < size && arr[i] < arr[left]){
-                swap(arr[i], arr[left]);
-                i = left;
+            int leftIndex = 2*i;
+            int rightIndex = 2*i + 1;
+
+            if(leftIndex < size && arr[i] < arr[leftIndex]){
+                swap(leftIndex, arr[leftIndex]);
+                i = leftIndex;
             }
-            else if(right < size && arr[i] < arr[right]){
-                swap(arr[i], arr[right]);
-                i = right;
+            else if(rightIndex < size && arr[i] < arr[rightIndex]){
+                swap(rightIndex, arr[rightIndex]);
+                i = rightIndex;
             }
             else{
                 return;
             }
         }
     }
+
+    // Heapify Algo
+    void heapify(int arr[], int size, int i){
+        int largest = i; 
+        int left = 2*i;
+        int right = 2*i + 1;
+
+        if(left < size && arr[largest] < arr[left]){
+            largest = left;
+        }
+        if(right < size && arr[largest] < arr[right]){
+            largest = right;
+        }
+
+        if( largest != i ){
+            swap(arr[largest], arr[i]);
+            heapify(arr, size, largest);
+        }
+    }
 };
-
-
-void heapifyzerobased(int arr[], int size, int i){
-    int largest = i;
-    int left = 2*i;
-    int right = (2*i) + 1;
-    
-    if(left <= size && largest < arr[left]){
-        largest = left;
-    }
-    if(right <= size && largest < arr[right]){
-        largest = right;
-    }
-    
-    if(largest != i){
-        swap(arr[largest], arr[i]);
-        heapify(arr, size, largest);
-    }
-}
-
-void heapifyonebased(int arr[], int size, int i){
-    int largest = i;
-    int left = 2*i;
-    int right = (2*i) + 1;
-    
-    if(left < size && largest < arr[left]){
-        largest = left;
-    }
-    if(right < size && largest < arr[right]){
-        largest = right;
-    }
-    
-    if(largest != i){
-        swap(arr[largest], arr[i]);
-        heapify(arr, size, largest);
-    }
-}
-
-void heapSort(int arr[i], int i){
-    int size = ;
-    
-    while(size > 1){
-        // step1
-        swap(arr[size], arr[i]);
-        size--;
-        
-        // step 2
-        heapifyzerobased(arr, size, i);
-    }
-}
 
 int main(){
     
     heap h;
-    // h.insert(50);
-    // h.insert(55);
-    // h.insert(53);
-    // h.insert(52);
-    // h.insert(54);
-    // h.print();
-    // h.deletefromheap();
-    // h.print();
-    
-    int arr[6] = {-1, 54, 53, 55, 52, 50};
-    int n = 5;
-    
-    for(int i=n/2; i>1; i--){
-        heapify(arr, n, i);
-    }
-    
-    cout << "printing" << endl;
-    
-    for(int i=1; i<n; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    
-    heapsort(arr,n);
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+    h.deleteFromHeap();
+    h.print();
     
     return 0;
 }
