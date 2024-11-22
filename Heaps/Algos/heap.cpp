879f9d7119c3ac6f9e8271438eaf4d056673ce7d
Pinak Kundu <pinakkundu1080@gmail.com>
2024-11-22 18:21:35 +0000

    added example array & restructured

--- Heaps/Algos/heap.cpp
+++ Heaps/Algos/heap.cpp
@@ -1,19 +1,22 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-class heap{
-    public:
+class heap
+{
+public:
     int arr[100];
     int size = 0;
 
-    heap(){
+    heap()
+    {
         arr[0] = -1;
-        size =0;
+        size = 0;
     }
 
     // Max Heap
-    void insert(int val){
-        // 1. insert at last index 
+    void insert(int val)
+    {
+        // 1. insert at last index
         // 2. compare with parent (index/2)
 
         // increase the size to insert at last index
@@ -21,29 +24,37 @@ class heap{
         int index = size;
         arr[index] = val;
 
-        while(index > 1){
-            int parent = index/2;
-            if(arr[parent] < arr[index]){
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
                 swap(arr[parent], arr[index]);
                 index = parent;
             }
-            else{
+            else
+            {
                 return;
             }
         }
     }
 
-    void print(){
-        for(int i=1; i<size; i++){
+    void print()
+    {
+        for (int i = 1; i < size; i++)
+        {
             cout << arr[i] << " ";
-        } cout << endl;
+        }
+        cout << endl;
     }
 
-    void deleteFromHeap(){
-        // 1. swap last index with root 
+    void deleteFromHeap()
+    {
+        // 1. swap last index with root
         // 2. remove last node
         // 3. to correct position
-        if(size == 0){
+        if (size == 0)
+        {
             cout << "nothing to delete";
             return;
         }
@@ -55,47 +66,56 @@ class heap{
         size--;
 
         // to correct position
-        int i=1;
-        while(i<size){
-            int leftIndex = 2*i;
-            int rightIndex = 2*i + 1;
+        int i = 1;
+        while (i < size)
+        {
+            int leftIndex = 2 * i;
+            int rightIndex = 2 * i + 1;
 
-            if(leftIndex < size && arr[i] < arr[leftIndex]){
+            if (leftIndex < size && arr[i] < arr[leftIndex])
+            {
                 swap(leftIndex, arr[leftIndex]);
                 i = leftIndex;
             }
-            else if(rightIndex < size && arr[i] < arr[rightIndex]){
+            else if (rightIndex < size && arr[i] < arr[rightIndex])
+            {
                 swap(rightIndex, arr[rightIndex]);
                 i = rightIndex;
             }
-            else{
+            else
+            {
                 return;
             }
         }
     }
-
-    // Heapify Algo
-    void heapify(int arr[], int size, int i){
-        int largest = i; 
-        int left = 2*i;
-        int right = 2*i + 1;
-
-        if(left < size && arr[largest] < arr[left]){
-            largest = left;
-        }
-        if(right < size && arr[largest] < arr[right]){
-            largest = right;
-        }
-
-        if( largest != i ){
-            swap(arr[largest], arr[i]);
-            heapify(arr, size, largest);
-        }
-    }
 };
 
-int main(){
-    
+// Heapify Algo
+void heapify(int arr[], int size, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < size && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if (right < size && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, size, largest);
+    }
+}
+
+int main()
+{
+
     heap h;
     h.insert(50);
     h.insert(55);
@@ -105,6 +125,23 @@ int main(){
     h.print();
     h.deleteFromHeap();
     h.print();
-    
+
+    int arr[6] = {
+        -1, 54, 53, 55, 52, 50};
+
+    int n = 5;
+
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    cout << "printing the heap:" << endl;
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
